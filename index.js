3cfd77e7628d0628baa6bb5bbd862b27e35a2f5f
Manoel Lima <manoelsslima@yahoo.com.br>
2024-04-29 00:32:14 -0400

    Add client

--- index.js
+++ index.js
@@ -3,6 +3,33 @@ const { stdin: input, stdout: output } = require('node:process');
 
 const rl = readline.createInterface({ input, output });
 
+const customers = [];
+
+// a função deve ser async sempre que for usar await dentro dela
+async function listCustomers() {
+    console.clear();
+    console.log(customers);
+    await rl.question("Pressione Enter para continuar...");
+    printMenu();
+}
+
+async function startRegistration() {
+    console.clear();
+    const name = await rl.question("Qual o nome do cliente?\n");
+    const address = await rl.question("Qual o endereço do cliente?\n");
+    const id = customers.length > 0 ? customers[customers.length - 1].id + 1 : 1;
+    // não precisa ser name: name; address: address; id: id. Se forem iguais
+    // o javascript vai assumir
+    customers.push({
+        id,
+        name,
+        address
+    });
+    console.log("Cliente cadastrado com sucesso!");
+    await rl.question("Pressione Enter para continuar...");
+    printMenu();
+}
+
 async function printMenu() {
     try {
         console.clear();
@@ -11,16 +38,19 @@ async function printMenu() {
         console.log("2 - Ver Clientes");
         console.log("3 - Encerrar");
         
-        const answer = await rl.question("Qual opção você deseja?");
-        if (answer === "3") {
-            process.exit(0);
-        } else {
-            console.log("Sua escolha foi: " + answer);
-            
-            //setTimeout(printMenu, 3000);
-            await rl.question("Pressione Enter para continuar...");
-            printMenu();
+        const answer = await rl.question("Qual opção você deseja? ");
+
+        switch(answer) {
+            case "1": startRegistration(); break;
+            case "2": listCustomers(); break;
+            case "3": {
+                console.clear();
+                process.exit(0);
+            }
+            default: console.log("Opção inválida. Tente novamente.");
         }
+        await rl.question("Pressione Enter para continuar...");
+        printMenu();
     } catch (err) {
         console.error("Resposta rejeitada", err);
     }
